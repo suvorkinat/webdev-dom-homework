73e413aa7002b175d475568cf935997e201d22f8
Tatiana Suvorkina <suvorkina_tanya@mail.ru>
2024-03-26 20:58:16 +0300

    изменено что-то

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 import {getPromise, postPromise} from "./api.js"
 import {renderComments} from "./render.js"
-import {initEventListeners, initEventAndCommentListener, answerComment} from "./listeners.js"
+import {initEventListeners, AddComment, answerComment} from "./listeners.js"
 import { normalizeComments } from "./helpers.js";
 const commentsLoading = document.querySelector('.data-loading');
 
@@ -9,7 +9,6 @@ const commentsLoading = document.querySelector('.data-loading');
 const inputNameElement = document.querySelector(".add-form-name");
 const inputTextElement = document.querySelector(".add-form-text");
 const buttonElement = document.querySelector(".add-form-button");
-const buttonElementDel = document.querySelector(".delete-form-button");
 // Переносим данные из разметки
 export let comments = [];
 export function setComments(newComments) {
@@ -29,7 +28,7 @@ export const fetchPromiseGet = () => {
   })
 };
 fetchPromiseGet();
-initEventAndCommentListener();
+AddComment();
 
 
 
@@ -44,10 +43,14 @@ inputTextElement.addEventListener("keydown", function (event) {
 });
 //удаление последнего комментария
 
-buttonElementDel.addEventListener("click", () => {
+const buttonElementDel = document.querySelector(".delete-form-button");
 
-  comments.pop();
-  renderComments({comments, initEventListeners, answerComment});
-});
-
-//console.log("It works!");*/
+if (buttonElementDel) {
+  buttonElementDel.addEventListener("click", () => {
+    comments.pop();
+    renderComments({comments, initEventListeners, answerComment});
+  });
+} else {
+  console.error("Элемент не найден в DOM");
+}
+//console.log("It works!");
